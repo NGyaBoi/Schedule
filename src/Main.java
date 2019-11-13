bd26d90071649988a0d18988d88e759b7090dc7d
NGyaBoi <NeelGhosh@protonmail.com>
2019-11-12 19:35:52 -0500

    Realigned Main description

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 /* Filename:		Main.java
- * Author:		Neel Ghosh
- * Section:		IST 140.001
+ * Author:			Neel Ghosh
+ * Section:			IST 140.001
  * Assignment:		Final Project
- * Date: 		Dec 10th 2019
+ * Date: 			Dec 10th 2019
  * Description:		[empty]
  */
 
