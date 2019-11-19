f8c3b227ea7c043c80e3c45c3939f27e0bfa886d
NGyaBoi <NeelGhosh@protonmail.com>
2019-11-18 20:44:38 -0500

    added GetName class

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,5 +1,11 @@
-/*
- * 
+import javax.swing.JOptionPane;
+
+/* Filename:		Schedule.java
+ * Author:			Yifan Feng, Neel Ghosh,  Seungyup Lee, Miloni Shah
+ * Section:			IST 140.001
+ * Assignment:		Final Project
+ * Date: 			Dec 12th 2019
+ * Description:		(empty)
  */
 
 public class Schedule 
@@ -9,5 +15,13 @@ public class Schedule
 	{
 		
 	}
-
+	
+	public static String GetName()
+	{
+		String strName = "";
+		
+		strName = JOptionPane.showInputDialog("Enter in your name:");
+		
+		return strName;
+	}
 }
